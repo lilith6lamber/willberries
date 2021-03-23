f00d0340f75c4655524628a3f8c04c6a20df2089
Lilith Lamber <winter.rituel@gmail.com>
2021-03-23 11:30:24 +0200

    cart, smooth scroll

--- js/main.js
+++ js/main.js
@@ -8,3 +8,41 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
+// cart
+
+const buttonCart = document.querySelector('.button-cart');
+const modalCart = document.querySelector('#modal-cart');
+//const modalClose = document.querySelector('.modal-close');
+
+const openModal = () => {
+	modalCart.classList.add('show')
+};
+const closeModal = () => {
+	modalCart.classList.remove('show')
+};
+
+buttonCart.addEventListener('click', openModal);
+modalCart.addEventListener('click', e => {
+	if (e.target.classList.contains('show') || e.target.classList.contains('modal-close')) {
+		closeModal()
+	}
+});
+
+
+// smooth scroll
+
+(function() {
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+	
+	for (let i = 0; i < scrollLinks.length; i++) {
+		scrollLinks[i].addEventListener('click', e => {
+			e.preventDefault();
+			const id = scrollLinks[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});
+		});
+	}
+})()
+
